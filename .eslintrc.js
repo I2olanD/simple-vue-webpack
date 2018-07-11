edaa9c8d13a46d7936de73d9f8c11025160bf5c1
Roland Olah <rolandolah91+github@gmail.com>
2018-07-11 17:16:18 +0200

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module',
+  },
+
+  extends: 'airbnb-base',
+
+  env: {
+    browser: true,
+    jest: true,
+    es6: true,
+  },
+
+  // required to lint *.vue files
+  plugins: [
+    'import',
+    'html',
+  ],
+
+  // check if imports actually resolve
+  settings: {
+    'import/resolver': 'webpack',
+  },
+
+  // add your custom rules here
+  rules: {
+    // don't require .vue extension when importing
+    'import/extensions': ['error', 'always', {
+      'js': 'never',
+      'vue': 'never',
+    }],
+  }
+}
---
+++ webpack.config.babel.js
@@ -0,0 +1,53 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const resolve = file => path.resolve(__dirname, file);
+const isProd = process.argv.indexOf('-p') !== -1;
+
+const config = {
+  mode: isProd ? 'production' : 'development',
+
+  entry: resolve('src/index.js'),
+
+  output: {
+    filename: 'index.js',
+    libraryTarget: 'umd',
+    library: 'SimpleVueWebpack',
+  },
+
+  resolve: {
+    extensions: ['.vue', '.js', '.json'],
+    alias: {
+      src: resolve('src'),
+    },
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|vue)$/,
+        enforce: 'pre',
+        use: 'eslint-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.vue$/,
+        use: 'vue-loader',
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: resolve('src/index.html'),
+      inject: true,
+    }),
+  ],
+};
+
+export default config;
